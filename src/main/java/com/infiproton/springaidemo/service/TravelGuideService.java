51555258d4885d127781db6b30beaf95fb8e1c32
Harish <harish.bn@gmail.com>
2025-09-04 13:58:12 +0530

    Implement external prompt templates using StringTemplate format

--- src/main/java/com/infiproton/springaidemo/service/TravelGuideService.java
+++ src/main/java/com/infiproton/springaidemo/service/TravelGuideService.java
@@ -3,6 +3,8 @@ package com.infiproton.springaidemo.service;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -11,15 +13,16 @@ import java.util.Map;
 public class TravelGuideService {
 
     private final ChatClient chatClient;
-    PromptTemplate template = new PromptTemplate(
-            "Create a {days}-day travel plan for {city}."
-    );
+    @Value("classpath:prompts/travel-guide.st")
+    private Resource travelGuideTemplate;
 
     TravelGuideService(ChatClient chatClient) {
         this.chatClient = chatClient;
     }
 
     public String prepareTravelPlan(String city, Integer days) {
+        PromptTemplate template = new PromptTemplate(travelGuideTemplate);
+
         Map<String, Object> params = Map.of(
                 "city", city,
                 "days", days
