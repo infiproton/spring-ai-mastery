68c29acc2e618a2070b21685dd1282f49ab5883b
Harish <harish.bn@gmail.com>
2025-09-07 16:04:13 +0530

    revert back to moduel2.5 content

--- src/main/java/com/infiproton/springaidemo/controller/ChatController.java
+++ src/main/java/com/infiproton/springaidemo/controller/ChatController.java
@@ -5,20 +5,14 @@ import com.infiproton.springaidemo.model.TravelPlan;
 import com.infiproton.springaidemo.service.ChatService;
 import com.infiproton.springaidemo.service.TravelGuideService;
 import org.springframework.ai.chat.memory.ChatMemory;
-import org.springframework.ai.chat.messages.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/ai")
 class ChatController {
-
     private final ChatService chatService;
     private final TravelGuideService travelGuideService;
-    @Autowired
-    private ChatMemory chatMemory;
 
     ChatController(ChatService chatService, TravelGuideService travelGuideService) {
         this.chatService = chatService;
@@ -30,13 +24,8 @@ class ChatController {
         return travelGuideService.prepareTravelPlan(city, days);
     }
 
-    @GetMapping("/memory")
-    public List<Message> fetchMemory() {
-        return chatMemory.get("default");
-    }
-
     @PostMapping("/chat")
-    public String chat(@RequestBody ChatRequest chatRequest){
+    public String chat(@RequestBody ChatRequest chatRequest) {
         return chatService.chat(chatRequest.getMessage());
     }
 }
