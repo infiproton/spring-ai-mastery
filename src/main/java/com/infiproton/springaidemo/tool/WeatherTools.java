531095baa758558d1fda619dfb2de493a155fd08
Harish <harish.bn@gmail.com>
2025-09-12 14:35:37 +0530

    Add weather API key configuration and refactor WeatherTools to use it

--- src/main/java/com/infiproton/springaidemo/tool/WeatherTools.java
+++ src/main/java/com/infiproton/springaidemo/tool/WeatherTools.java
@@ -1,21 +1,19 @@
 package com.infiproton.springaidemo.tool;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.infiproton.springaidemo.model.ForecastResponse;
-import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
-
-import java.util.List;
 
 @Component
 @Slf4j
 public class WeatherTools {
     private final RestTemplate restTemplate = new RestTemplate();
-    private static final String API_KEY = "b686f8b1e87349559e9140605250308";
+    @Value("${app.weather.api-key}")
+    private String apiKey;
 
     @Tool(description = "Get weather forecast for a given city and date (yyyy-MM-dd). If date is not provided, defaults to today.")
     public String getWeather(String city, String date) {
@@ -23,13 +21,13 @@ public class WeatherTools {
             // Build API URL
             String url = UriComponentsBuilder
                     .fromHttpUrl("http://api.weatherapi.com/v1/forecast.json")
-                    .queryParam("key", API_KEY)
+                    .queryParam("key", apiKey)
                     .queryParam("q", city)
                     .queryParam("dt", date)
                     .toUriString();
 
             ForecastResponse apiResponse = restTemplate.getForObject(url, ForecastResponse.class);
-            if (apiResponse  == null) {
+            if (apiResponse == null) {
                 return "No response from Weather API";
             }
 
