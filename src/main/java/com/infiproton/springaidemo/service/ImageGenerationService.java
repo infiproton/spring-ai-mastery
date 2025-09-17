53cce0b45f03f3be32cd14a83dd017fdb2699ff6
Harish <harish.bn@gmail.com>
2025-09-17 18:29:28 +0530

    open AI image generation

--- src/main/java/com/infiproton/springaidemo/service/ImageGenerationService.java
+++ src/main/java/com/infiproton/springaidemo/service/ImageGenerationService.java
@@ -3,9 +3,7 @@ package com.infiproton.springaidemo.service;
 import org.springframework.ai.image.ImageModel;
 import org.springframework.ai.image.ImagePrompt;
 import org.springframework.ai.image.ImageResponse;
-import org.springframework.ai.stabilityai.StyleEnum;
-import org.springframework.ai.stabilityai.api.StabilityAiImageOptions;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.ai.openai.OpenAiImageOptions;
 import org.springframework.stereotype.Service;
 
 import java.util.Base64;
@@ -14,14 +12,13 @@ import java.util.Base64;
 public class ImageGenerationService {
     private final ImageModel imageModel;
 
-    public ImageGenerationService(@Qualifier("stabilityAiImageModel") ImageModel imageModel) {
+    public ImageGenerationService(ImageModel imageModel) {
         this.imageModel = imageModel;
     }
 
     public byte[] generate(String message) {
         ImageResponse imageResponse = imageModel.call(new ImagePrompt(message,
-                StabilityAiImageOptions.builder()
-                        .stylePreset(StyleEnum.CINEMATIC)
+                OpenAiImageOptions.builder()
                         .responseFormat("b64_json")
                         .build()));
         String b64 = imageResponse.getResult().getOutput().getB64Json();
